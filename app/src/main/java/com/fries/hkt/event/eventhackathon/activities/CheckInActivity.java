7661f4e141910ddd057951635e6217d8c2005212
quytm <quytm_58@vnu.edu.vn>
2017-03-10 23:04:55 +0700

    Quét QR code, gõ code -> Join vào sự kiện

--- app/src/main/java/com/fries/hkt/event/eventhackathon/activities/CheckInActivity.java
+++ app/src/main/java/com/fries/hkt/event/eventhackathon/activities/CheckInActivity.java
@@ -1,20 +1,188 @@
 package com.fries.hkt.event.eventhackathon.activities;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.Display;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.fries.hkt.event.eventhackathon.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 /**
  * Created by tmq on 10/03/2017.
  */
 
-public class CheckInActivity extends AppCompatActivity {
+public class CheckInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = CheckInActivity.class.getSimpleName();
+
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private SurfaceView cameraView;
+    private EditText edtTypeCode;
+    private ImageButton btnSendCode;
+
+    private boolean allowScan = true;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            String code = bundle.getString("code");
+
+            Log.i(TAG, "code = " + code);
+            checkCode(code);
+            Toast.makeText(CheckInActivity.this, code, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
+
+        initViews();
+        initCamera();
     }
+
+    private void initViews() {
+        cameraView = (SurfaceView) findViewById(R.id.camera_view);
+        edtTypeCode = (EditText) findViewById(R.id.edt_type_code);
+        btnSendCode = (ImageButton) findViewById(R.id.btn_send_code);
+
+        btnSendCode.setOnClickListener(this);
+    }
+
+    private void initCamera() {
+        barcodeDetector =
+                new BarcodeDetector.Builder(this)
+                        .setBarcodeFormats(Barcode.QR_CODE)
+                        .build();
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(size.y, size.x)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(CheckInActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ie) {
+                    Log.e("CAMERA SOURCE", ie.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                if (!allowScan) return;
+
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    allowScan = false;
+                    Message message = mHandler.obtainMessage();
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString("code", barcodes.valueAt(0).displayValue);
+
+                    message.setData(bundle);
+                    mHandler.sendMessage(message);
+                }
+            }
+        });
+    }
+
+    private void checkCode(String code) {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage(getString(R.string.txt_please_wait));
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+
+
+        { // Run on Thread
+            Toast.makeText(CheckInActivity.this, R.string.txt_login_success, Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();
+
+            Intent intent = new Intent(CheckInActivity.this, MainActivity.class);
+            startActivity(intent);
+
+            CheckInActivity.this.finish();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_send_code:
+                String code = edtTypeCode.getText().toString();
+                if (!code.isEmpty()) {
+                    checkCode(code);
+                } else {
+                    Toast.makeText(this, "Chưa điền mã", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraSource.release();
+        barcodeDetector.release();
+    }
+
 }
