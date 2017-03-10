a4f835075d6253ed974a130934dd23f403b29157
quytm <quytm_58@vnu.edu.vn>
2017-03-10 20:03:50 +0700

    Thêm trang Check In

---
+++ app/src/main/java/com/fries/hkt/event/eventhackathon/activities/CheckInActivity.java
@@ -0,0 +1,20 @@
+package com.fries.hkt.event.eventhackathon.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fries.hkt.event.eventhackathon.R;
+
+/**
+ * Created by tmq on 10/03/2017.
+ */
+
+public class CheckInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in);
+    }
+}
