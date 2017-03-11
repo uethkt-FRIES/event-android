4997d1d7d1a2c8b658fcf55fb61f862049b0d904
minhnt58 <tienminh.uet@gmail.com>
2017-03-11 13:08:18 +0700

    init dialog

---
+++ app/src/main/java/com/fries/hkt/event/eventhackathon/dialogs/TimelineDialog.java
@@ -0,0 +1,16 @@
+package com.fries.hkt.event.eventhackathon.dialogs;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * Created by TooNies1810 on 3/11/17.
+ */
+
+public class TimelineDialog extends Dialog {
+    public TimelineDialog(Context context) {
+        super(context);
+    }
+
+
+}
